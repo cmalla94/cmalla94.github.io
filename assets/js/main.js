879248b744e001390221ccd8482e9b93a63444fc
Chad <cmalla@sfu.com>
2019-07-05 14:52:31 -0700

    changes to intro

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@
 	// Play initial animations on page load.
 		$window.on('load', function() {
 			window.setTimeout(function() {
-				$body.removeClass('is-preload');
+				$body.removeClass('is-preload'); 
 			}, 100);
 		});
 
